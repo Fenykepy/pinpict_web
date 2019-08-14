ee49cb4868d6ecf6727469c5b1627da6cb3cf525
Fenykepy <phiroom@lavilotte-rolle.fr>
2019-08-14 15:53:02 +0200

    Introduces redux.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStoreWithMiddleware } from './store'
+import rootReducer from './rootReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css'
+import App from './app/containers/app/App'
+import * as serviceWorker from './serviceWorker'
+
+let store = createStoreWithMiddleware(rootReducer)
+
+// Log the initial state
+console.log(store.getState())
+
+// Everytime the state changes, log it
+store.subscribe(() => console.log(store.getState()))
+
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
