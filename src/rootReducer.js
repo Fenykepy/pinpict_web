b0e15366e3ea7113416299cf750cdea4660bc519
Fenykepy <pro@lavilotte-rolle.fr>
2019-11-01 19:04:42 +0100

    add fake routes and reducers.

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -11,3 +11,84 @@ const rootReducer = combineReducers({
 })
 
 export default rootReducer
+
+
+
+/* fake store */
+
+/*
+
+pins = {
+  id: {
+    id: int,
+    fetched: bool,
+    is_fetching: bool,
+    sha1: string,
+    source_file: string,
+    source_file_url: string,
+    source_domain: string,
+    source: string,
+    date_created: datetime,
+    description: string,
+    added_via: <username>,
+    owner_rate: int,
+    n_likes: int,
+  }
+}
+
+scan = {
+  url: string,
+  full_search: bool,
+  is_scanning: bool,
+  results: [
+    {src: string, description: string},
+  ]
+}
+
+tags = ["name", "name"]
+
+boards = {
+  slug@userslug: {
+    slug: string,
+    short_fetched: bool,
+    is_fetching_short: bool,
+    full_fetched: bool,
+    is_fetching_full: bool,
+
+    // short data
+    title: string,
+    slug: string,
+    n_pins: int,
+    cover1: string,
+    cover2: string,
+    cover3: string,
+    cover4: string,
+    cover5: string,
+
+    // full data
+    description: string,
+    pin_default_description: string,
+    pins_order: string,
+    reverse_pins_order: bool,
+    users_can_read: [],
+    followers: [],
+    n_followers: int,
+    pins: []
+  }
+}
+
+users = {
+  userslug: {
+    fetched: bool,
+    is_fetching: bool,
+    slug: string,
+    username: slug,
+    avatar: string,
+    website: string,
+    ...socialmedias
+  }
+}
+
+currentuser = userslug
+
+*/
--- src/route.js
+++ src/route.js
@@ -22,3 +22,30 @@ export default () => {
     </Route>
   )
 }
+
+
+/* URL's */
+
+/*
+   "/"                        Last public pin's list.
+   "/login/?next=<redirect>"  Login form, redirect to "/<userslug>" after if no next query string
+   "/<userslug>/"             User board list
+   "/<userslug>/<boardslug>/" Board pin's list
+   "/<userslug>/pins/"        User pins list
+   "/<userslug>/followers/"   User followers list
+   "/<userslug>/following/"   User following list
+   "/pin/<pin_id>/"           Pin detail view
+   "/pin/find/<tag>/"         Pins list filtered by tag
+   "/pin/scan/?url=<url>"     Scan url for images
+*/
+
+
+/* Modal navigation */
+
+/*
+   Board creation
+   Board edition
+   Pin creation
+   Pin edition
+   Profile edition
+*/
